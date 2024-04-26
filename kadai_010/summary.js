31e309a60cb20a031fcd58457e340cdd54ef372e
takada2024 <165872660+takada2024@users.noreply.github.com>
2024-04-26 12:02:41 +0900

    Update summary.js

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -7,9 +7,11 @@ $(function () {
     $('#change-text').on('click', function () {
         $('#target').text('Hello!');
     });
+    
     $('#fade-out').on('click', function () {
         $('#target').fadeOut();
     });
+    
     $('#fade-in').on('click', function () {
         $('#target').fadeIn();
     });
